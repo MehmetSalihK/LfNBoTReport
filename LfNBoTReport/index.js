cf8a66a6e53931f7fb60650daa73bf65c2b0d1f0
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-04-25 18:31:37 +0200

    Update index.js

--- LfNBoTReport/index.js
+++ LfNBoTReport/index.js
@@ -17,7 +17,7 @@ bot.on("ready", () => {
 
 bot.on('guildMemberAdd', function (member) {
     member.createDM().then(function (channel) {
-        return channel.send('__***:heavy_check_mark::red_circle:BIENVENUE:red_circle::heavy_check_mark:***__' +n\ member.displayName)
+        return channel.send('__***:heavy_check_mark::red_circle:BIENVENUE:red_circle::heavy_check_mark:***__' +\n member.displayName)
  
     }).catch(console.error)
 })
